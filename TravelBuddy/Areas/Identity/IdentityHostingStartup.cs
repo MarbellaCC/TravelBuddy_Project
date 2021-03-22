94cc77a67bfc0c2e3c6642b3806ecb901ee1f0ba
marbella allan <camposmarbella898029@ymail.com>
2021-03-22 16:02:56 -0500

    Implemented roles

---
+++ TravelBuddy/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TravelBuddy.Data;
+
+[assembly: HostingStartup(typeof(TravelBuddy.Areas.Identity.IdentityHostingStartup))]
+namespace TravelBuddy.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ TravelBuddy/Models/Traveler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelBuddy.Models
+{
+    public class Traveler
+    {
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserID { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
